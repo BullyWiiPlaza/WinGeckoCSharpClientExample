650a4daf176c518dfa7f2dac76d59d500d611305
BullyWiiPlaza <bullywiihacks@gmail.com>
2021-09-02 21:00:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WinGecko;
+
+namespace WinGeckoCSharpClientExample
+{
+    internal class Program
+    {
+        private const string WinGeckoServerFilePath = "your-WinGeckoServer.exe-file-path";
+
+        private static void Main()
+        {
+            using (var winGeckoClient = WinGeckoClient.ConnectWinGeckoClient(WinGeckoServerFilePath))
+            {
+                var serverDetails = winGeckoClient.ReadServerDetails();
+                Console.WriteLine(serverDetails.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
